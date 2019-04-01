dec4f4f7d69c3b548a6c2210890c95f14e592c08
Hannes Mehnert <hannes@mehnert.org>
2019-04-01 22:44:40 +0200

    decoder and encoder of basics

--- src/packet.ml
+++ src/packet.ml
@@ -3,6 +3,19 @@
 
    no support for key method v1! *)
 
+open Rresult.R.Infix
+
+type error = [
+  | `Partial
+  | `Leftover of int
+  | `Unknown_operation of int
+]
+
+let pp_error ppf = function
+  | `Partial -> Fmt.string ppf "partial"
+  | `Leftover off -> Fmt.pf ppf "leftover at %d" off
+  | `Unknown_operation op -> Fmt.pf ppf "unknown operation %d" op
+
 type operation =
   | Soft_reset
   | Control
@@ -32,44 +45,132 @@ let pp_operation ppf op =
 
 type packet_id = int32 (* 4 or 8 bytes -- latter in pre-shared key mode *)
 
+let packet_id_len = 4
+
 let hmac_len = 20 (* SHA1 is what you say *)
 
-type packet = {
-  (* 16 bit length *)
-  operation : operation ; (* uint8 *)
-  payload : Cstruct.t ;
-}
+let hdr_len = 8 + hmac_len + packet_id_len + 4 + 1
 
 let guard f e = if f then Ok () else Error e
 
-type error = [
-  | `Partial
-  | `Leftover
-  | `Unknown_operation of int
-]
-
-let decode_packet buf =
-  let open Rresult.R.Infix in
-  guard (Cstruct.len buf >= 3) `Partial >>= fun () ->
-  let plen = Cstruct.BE.get_uint16 buf 0 in
-  let op = Cstruct.get_uint8 buf 2 in
-  guard (Cstruct.len buf >= plen + 3) `Partial >>= fun () ->
-  guard (Cstruct.len buf = plen + 3) `Leftover >>= fun () ->
-  let payload = Cstruct.sub buf 0 plen in
-  int_to_operation op >>| fun operation ->
-  { operation ; payload }
-
-type control = {
+type header = {
   local_session : int64 ;
   hmac : Cstruct.t ; (* usually 16 or 20 bytes *)
   packet_id : packet_id ;
   timestamp : int32 ;
   (* uint8 array length *)
-  ack_packet_id : packet_id array ;
+  ack_packet_ids : packet_id list ;
   remote_session : int64 option ; (* if above is non-empty *)
-  message_packet_id : packet_id ; (* if op != ACK *)
-  payload : Cstruct.t ; (* if op != ACK *)
 }
+
+let pp_header ppf hdr =
+  Fmt.pf ppf "local %Lu packet_id %ld timestamp %ld hmac %a ack %a remote %a"
+    hdr.local_session hdr.packet_id hdr.timestamp Cstruct.hexdump_pp hdr.hmac
+    Fmt.(list ~sep:(unit ", ") int32) hdr.ack_packet_ids
+    Fmt.(option ~none:(unit " ") int64) hdr.remote_session
+
+let decode_header buf =
+  guard (Cstruct.len buf >= hdr_len) `Partial >>= fun () ->
+  let local_session = Cstruct.BE.get_uint64 buf 0
+  and hmac = Cstruct.sub buf 8 hmac_len
+  and packet_id = Cstruct.BE.get_uint32 buf (hmac_len + 8)
+  and timestamp = Cstruct.BE.get_uint32 buf (hmac_len + 12)
+  and arr_len = Cstruct.get_uint8 buf (hmac_len + 16)
+  in
+  guard (Cstruct.len buf >= hdr_len + packet_id_len * arr_len + 8) `Partial >>| fun () ->
+  let rec ack_packet_id = function
+    | 0 -> []
+    | n ->
+      let id = Cstruct.BE.get_uint32 buf (hdr_len + packet_id_len * n) in
+      id :: (ack_packet_id (pred n))
+  in
+  let ack_packet_ids = ack_packet_id arr_len in
+  let remote_session =
+    if arr_len > 0 then
+      Some (Cstruct.BE.get_uint64 buf (hdr_len + packet_id_len * arr_len))
+    else
+      None
+  in
+  { local_session ; hmac ; packet_id ; timestamp ; ack_packet_ids ; remote_session },
+  (hdr_len + packet_id_len * arr_len + 8)
+
+let encode_header hdr =
+  let id_arr_len = packet_id_len * List.length hdr.ack_packet_ids in
+  let rsid = if id_arr_len = 0 then 0 else 8 in
+  let buf = Cstruct.create (hdr_len + rsid + id_arr_len) in
+  Cstruct.BE.set_uint64 buf 0 hdr.local_session ;
+  Cstruct.blit hdr.hmac 0 buf 8 hmac_len ;
+  Cstruct.BE.set_uint32 buf (hmac_len + 8) hdr.packet_id ;
+  Cstruct.BE.set_uint32 buf (hmac_len + 12) hdr.timestamp ;
+  Cstruct.set_uint8 buf (hmac_len + 16) (List.length hdr.ack_packet_ids);
+  List.iteri
+    (fun i v -> Cstruct.BE.set_uint32 buf (hmac_len + 17 + i * packet_id_len) v)
+    hdr.ack_packet_ids ;
+  (match hdr.remote_session with
+   | None -> ()
+   | Some v ->
+     assert (rsid <> 0) ;
+     Cstruct.BE.set_uint64 buf (hdr_len + id_arr_len) v) ;
+  buf, hdr_len + rsid + id_arr_len
+
+type control = header * packet_id * Cstruct.t
+
+let pp_control ppf (hdr, id, payload) =
+  Fmt.pf ppf "%a id %lu %a" pp_header hdr id Cstruct.hexdump_pp payload
+
+let decode_control buf =
+  decode_header buf >>= fun (header, off) ->
+  guard (Cstruct.len buf >= off + 4) `Partial >>| fun () ->
+  let packet_id = Cstruct.BE.get_uint32 buf off
+  and payload = Cstruct.shift buf 4
+  in
+  (header, packet_id, payload)
+
+let encode_control (header, packet_id, payload) =
+  let hdr_buf, len = encode_header header in
+  let packet_id_buf = Cstruct.create 4 in
+  Cstruct.BE.set_uint32 packet_id_buf 0 packet_id ;
+  Cstruct.concat [ hdr_buf ; packet_id_buf ; payload ],
+  len + Cstruct.len payload + 4
+
+let decode_data buf = Ok buf
+
+let encode_data data = data, Cstruct.len data
+
+let decode buf =
+  guard (Cstruct.len buf >= 3) `Partial >>= fun () ->
+  let plen = Cstruct.BE.get_uint16 buf 0 in
+  let op = Cstruct.get_uint8 buf 2 in
+  guard (Cstruct.len buf >= plen + 3) `Partial >>= fun () ->
+  guard (Cstruct.len buf = plen + 3) (`Leftover (plen + 3)) >>= fun () ->
+  let payload = Cstruct.sub buf 0 plen in
+  int_to_operation op >>| fun operation ->
+  match operation with
+  | Ack -> decode_header payload >>| fun (ack, _) -> `Ack ack
+  | Data_v1 | Data_v2 -> decode_data payload >>| fun data -> `Data (operation, data)
+  | _ -> decode_control payload >>| fun control -> `Control (operation, control)
+
+let encode p =
+  let (payload, len), operation = match p with
+    | `Ack ack -> encode_header ack, Ack
+    | `Control (operation, control) -> encode_control control, operation
+    | `Data (operation, d) -> encode_data d, operation
+  in
+  let buf = Cstruct.create 3 in
+  Cstruct.BE.set_uint16 buf 0 len ;
+  Cstruct.set_uint8 buf (operation_to_int operation) 2 ;
+  Cstruct.append buf payload
+
+type t = [
+  | `Ack of header
+  | `Control of operation * control
+  | `Data of operation * Cstruct.t
+]
+
+let pp ppf = function
+  | `Ack a -> Fmt.pf ppf "ack %a" pp_header a
+  | `Control (op, c) -> Fmt.pf ppf "control %a: %a" pp_operation op pp_control c
+  | `Data (op, d) -> Fmt.pf ppf "data %a: %a" pp_operation op Cstruct.hexdump_pp d
 
 type tls_control = { (* v2 only! *)
   (* 4 zero bytes *)
