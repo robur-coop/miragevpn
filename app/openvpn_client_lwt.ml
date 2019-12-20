74953a0bd753e2be2acfbb8d807e17b5ce418e74
Hannes Mehnert <hannes@mehnert.org>
2019-12-20 12:45:05 +0100

    move read_len = 0 check below sa = sa' as suggested by @cfcs

--- app/openvpn_client_lwt.ml
+++ app/openvpn_client_lwt.ml
@@ -130,12 +130,13 @@ let read_udp =
   fun (sa, fd) ->
     Lwt_result.catch (
       Lwt_unix.recvfrom fd buf 0 bufsize [] >>= fun (count, sa') ->
-      if count = 0 then
-        Lwt.fail_with "end of file from server"
-      else if sa = sa' then
-        let cs = Cstruct.of_bytes ~len:count buf in
-        Logs.debug (fun m -> m "read %d bytes" count) ;
-        Lwt.return (Some cs)
+      if sa = sa' then
+        if count = 0 then
+          Lwt.fail_with "end of file from server"
+        else
+          let cs = Cstruct.of_bytes ~len:count buf in
+          Logs.debug (fun m -> m "read %d bytes" count) ;
+          Lwt.return (Some cs)
       else
         let pp_sockaddr ppf = function
           | Unix.ADDR_UNIX s -> Fmt.pf ppf "unix %s" s
