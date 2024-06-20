3e35f60f242f4bd7d8ad21792069d33649d8b357
Robur Team <team@robur.coop>
2024-06-20 15:05:36 +0000

    Add a test suite for miragevpn client<->openvpn
    The test script sets up a openvpn server with varying configuration
    and runs miragevpn-client-notun --test against the server.
    
    Co-Authored-By: Reynir Björnsson <reynir@reynir.dk>
    Co-Authored-By: Hannes Mehnert <hannes@mehnert.org>

---
+++ test/e2e/miragevpn-client.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -ex
+
+usage () {
+	echo "$1 <tun-interface> <configuration-dir>"
+	exit 1
+}
+
+if [ "$#" -ne 2 ]; then
+	usage
+fi
+
+tun_interface="$1"
+config_dir="$2"
+server_config="${config_dir}/server.conf"
+client_config="${config_dir}/client.conf"
+
+# run openvpn server
+pidfile="/tmp/miragevpn-e2e.pid"
+openvpn --cd "$config_dir" --config "server.conf" --dev-type tun --dev "$tun_interface" --writepid "$pidfile" &
+
+# kill openvpn server and report test status
+cleanup () {
+	cat "$pidfile" | xargs kill
+	rm "$pidfile"
+}
+trap cleanup EXIT
+
+sleep 0.5
+
+# run miragevpn-client-notun
+# NOTE: timeout as in FreeBSD 14 & GNU coreutils
+timeout -k 30 10 ../../_build/default/app/miragevpn_client_notun.exe --test "$client_config" > /dev/null
